cc18807bf93524f9bd77c660b328c50e6872f659
cacomoel <157077910+cacomoel@users.noreply.github.com>
2024-03-13 18:39:16 -0400

    update ui

--- Ui.lua
+++ Ui.lua
@@ -1,75 +1,93 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 
-local GUI = Library:create{
-    Theme = Library.Themes.Serika
-}
-
-local tab = GUI:tab{
-    Icon = "rbxassetid://6034996695",
-    Name = "Aimbot"
-}
-
-tab:button({
-    Name = "show prompt",
-    Callback = function()
-        tab:prompt{
-            Title = "baby",
-            Text = "shark doo doo doo doo im blank lmao",
-            Buttons = {
-                Ok = function()
-                    tab:prompt{
-                        Followup = true,
-                        Title = "really?",
-                        Text = "you sure?=",
-                        Buttons = {
-                            Yes = function()
-                                tab:prompt{
-                                    Followup = true,
-                                    Title = "xd",
-                                    Text = "sus",
-                                    Buttons = {
-                                        balls = function()
-                                            GUI:set_status("github")
-                                        end,
-                                        anal = function()
-                                            GUI:set_Status("money")
-                                        end
-                                    }
-                                }
-                            end,
-                        }
-                    }
-                end,
-            }
-        }
-    end,
-})
-tab:keybind({Callback = function()
-    GUI:prompt()
-end,})
-tab:dropdown({
-    Name = "Dropdown",
-    Description = "yeeeeeeeeeeeeeeeeeeeboi",
-    StartingText = "Bodypart",
-    Items = {
-        "Head",
-        "Torso",
-        "Random"
+-- check game
+if game.PlaceID == then
+    GUI:Notification{
+        Title = "Alert",
+        Text = "You shall bump the thread on V3rmillion!",
+        Duration = 3,
+        Callback = function() end
     }
-})
-local cum = tab:slider({Callback = function(v)
-    GUI:set_status(v)
-end})
+    -- call the script here
+end -- continue with ellse ifs
+-- end of game supported checking
 
-tab:textbox({Callback = function(v)
-    GUI:prompt{Text = v}
-end,})
+-- innitiate the gui interface
+local GUI = Mercury:Create{
+    Name = "Mercury",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+-- innitate the tabs
+local Chars = GUI:Tab{
+	Name = "Character",
+	Icon = "rbxassetid://8569322835"
+}
 
-tab:color_picker({
-    Name = "Color Of the Hub",
-    Style = Library.ColorPickerStyles.Legacy,
-    Description = "Click to adjust color...",
-    Callback = function(color)
-        print(color)
-    end,
-})
+local Scripts = GUI:Tab{
+	Name = "Scripts",
+	Icon = "rbxassetid://8569322835"
+}
+
+local Other = GUI:Tab{
+	Name = "Other",
+	Icon = "rbxassetid://8569322835"
+}
+-- chars gui
+
+-- walkspeed and jumpower
+local wss
+local jps
+local ws
+local jp
+
+Chars:Toggle{
+	Name = "WalkSpeed",
+	StartingState = false,
+	Description = "Set a custom walkspeed",
+	Callback = function(state) 
+        if state == true then
+            wss = true
+        else
+            wss = false
+    end
+}
+
+Chars:Slider{
+	Name = "Speed",
+	Default = 17,
+	Min = 0,
+	Max = 100,
+	Callback = function(v)
+        if wss == true then
+            ws = v
+            humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid")
+            humanoid.WalkSpeed = ws
+    end
+}
+
+Chars:Toggle{
+	Name = "JumpPower",
+	StartingState = false,
+	Description = "Set a custom walkspeed",
+	Callback = function(state) 
+        if state == true then
+            jps = true
+        else
+            jps = false
+    end
+}
+
+Chars:Slider{
+	Name = "Jump",
+	Default = 17,
+	Min = 0,
+	Max = 100,
+	Callback = function(v)
+        if jps == true then
+            jp = v
+            humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid")
+            humanoid.WalkSpeed = jp
+    end
+}
